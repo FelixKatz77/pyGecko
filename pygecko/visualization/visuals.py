be24112d2965d6d35109d335eef826567d4ca2e3
Felix Katzenburg <f_katz01@uni-muenster.de>
2023-12-12 14:21:50 +0100

    Add visualization of stacked chromatograms and spectra comparison
    The visualization module can now plot stacked chromatograms and the comparison of MS spectra.

--- pygecko/visualization/visuals.py
+++ pygecko/visualization/visuals.py
@@ -7,10 +7,11 @@ from matplotlib.collections import PatchCollection
 import matplotlib
 from matplotlib.ticker import (MultipleLocator)
 from matplotlib.figure import figaspect
+import matplotlib.gridspec as grid_spec
 from pygecko.visualization.utilities import yield_cmap
 
 if TYPE_CHECKING:
-    from pygecko.gc_tools import FID_Injection, MS_Injection, MS_Peak
+    from pygecko.gc_tools import Injection, FID_Injection, MS_Injection, MS_Peak
 
 plt.rcParams["font.family"] = 'sans-serif'
 plt.rcParams['font.sans-serif'] = ['Arial']
@@ -91,7 +92,7 @@ class Visualization:
 
 
     @staticmethod
-    def view_chromatogram(injection:Union['MS_Injection', 'FID_Injection'], path:str|None=None, **kwargs) -> None:
+    def view_chromatogram(injection:Union['MS_Injection', 'FID_Injection'], path:str|None=None, **kwargs):
 
         '''
         Visualizes a chromatogram as time/intensity plot.
@@ -118,7 +119,8 @@ class Visualization:
 
         w, h = figaspect(0.5)
         fig, ax = plt.subplots(figsize=(w, h))
-        xlim_scans = Utilities.convert_time_to_scan([x - injection.solvent_delay for x in xlim], injection.analysis_settings.scan_rate)
+        xlim_scans = Utilities.convert_time_to_scan([x - injection.solvent_delay for x in xlim],
+                                                    injection.analysis_settings.scan_rate)
 
         x, y = x[xlim_scans[0]:xlim_scans[1]], y[xlim_scans[0]:xlim_scans[1]]
 
@@ -129,7 +131,7 @@ class Visualization:
                              color='#005573', alpha=0.1, transform=ax.get_xaxis_transform())
 
 
-        plt.grid(color='grey', linestyle='--', which='both')
+        plt.grid(color='lightgrey', linestyle='--', which='both')
 
         ax.set_xlim(xlim)
         ax.set_ylim(ylim)
@@ -161,23 +163,25 @@ class Visualization:
 
         xlim = kwargs.pop('xlim', [None, None])
         ylim = kwargs.pop('ylim', [None, None])
-        color = kwargs.pop('color', '#000a64')
+        color = kwargs.pop('color', '#005573')
 
         fig, ax = plt.subplots(figsize=(14.4, 4.8))
-        ax.bar(peak.mass_spectrum['mz'], peak.mass_spectrum['rel_intensity'], width=0.05, color=color, edgecolor=color, **kwargs)
+        ax.bar(peak.mass_spectrum['mz'], peak.mass_spectrum['rel_intensity'], width=0.05, color=color, edgecolor=color,
+               **kwargs)
 
         lable_indices = np.argpartition(peak.mass_spectrum['rel_intensity'], -4)[-4:]
         for index in lable_indices:
-            ax.annotate(f'{peak.mass_spectrum["mz"][index]:.0f}', (peak.mass_spectrum['mz'][index], peak.mass_spectrum['rel_intensity'][index]),
-                        textcoords="offset points", xytext=(0,5), ha='center', color='darkgrey', fontsize=10)
+            ax.annotate(f'{peak.mass_spectrum["mz"][index]:.0f}',
+                        (peak.mass_spectrum['mz'][index], peak.mass_spectrum['rel_intensity'][index]),
+                        textcoords="offset points", xytext=(0, 5), ha='center', color='darkgrey', fontsize=10)
             print(peak.mass_spectrum['rel_intensity'][index])
 
         ax.set_xlim(xlim)
         ax.set_ylim(ylim)
 
-        plt.grid(color='grey', linestyle='--', which='both', axis='y')
+        plt.grid(color='lightgrey', linestyle='--', which='both', axis='y')
         ax.set_xlabel('m/z')
-        ax.set_ylabel('Intensity [%]')
+        ax.set_ylabel('Relative Intensity [%]')
         ax.spines[['right', 'top']].set_visible(False)
         fig.tight_layout()
 
@@ -186,6 +190,135 @@ class Visualization:
             plt.close()
         else:
             plt.show()
+
+    @staticmethod
+    def stack_chromatograms(injections:list['Injection'], path:str|None=None, **kwargs) -> None:
+
+        '''
+        Visualizes a list of chromatograms as a stack plot.
+
+        Args:
+            injections (list['Injection']): List of Injection objects containing the chromatograms to visualize.
+            path (str|None, optional): Path to save the figure to. Defaults to None.
+            **kwargs: Keyword arguments for the plot.
+        '''
+
+        color = kwargs.pop('color', '#005573')
+        linewidth = kwargs.pop('linewidth', 1)
+
+        gs = (grid_spec.GridSpec(len(injections), 1))
+        fig = plt.figure(figsize=(8, 6))
+
+        ax_objs = []
+
+        for i, injection in enumerate(injections):
+
+            if isinstance(color, list) and len(color) == len(injections):
+                color_i = color[i]
+            elif isinstance(color, str):
+                color_i = color
+            else:
+                color_i = '#005573'
+
+            ax = fig.add_subplot(gs[i:i+1, 0:])
+
+            raw = kwargs.pop('raw', False)
+
+            if raw or injection.detector == 'MS':
+                chromatogram = injection.chromatogram
+            else:
+                chromatogram = injection.processed_chromatogram
+            x = chromatogram[0]
+            y = chromatogram[1]
+
+            xlim = kwargs.pop('xlim', [x.min(), x.max()])
+            ylim = kwargs.pop('ylim', [-100000, None])
+
+            xlim_scans = Utilities.convert_time_to_scan([x - injection.solvent_delay for x in xlim],
+                                                        injection.analysis_settings.scan_rate)
+            x, y = x[xlim_scans[0]:xlim_scans[1]], y[xlim_scans[0]:xlim_scans[1]]
+
+            ax.plot(x, y, color=color_i, lw=linewidth, **kwargs)
+            ax.set_xlim(xlim)
+            ax.set_ylim(ylim)
+
+            ax.xaxis.set_major_locator(MultipleLocator(1.0))
+            ax.xaxis.set_minor_locator(MultipleLocator(0.5))
+            plt.grid(color='lightgrey', linestyle='--', which='both', axis='x')
+            if i == len(injections) - 1:
+                ax.set_xlabel('m/z')
+            if i != len(injections) - 1:
+                ax.set_xticklabels([])
+            ax.set_ylabel(f'{injection.sample_name}')
+            ax.spines[['right', 'top']].set_visible(False)
+
+            ax_objs.append(ax)
+
+        plt.tight_layout()
+
+        if path:
+            plt.savefig(path, dpi=400)
+            plt.close()
+        else:
+            plt.show()
+
+    @staticmethod
+    def compare_mass_spectra(peaks:tuple['MS_Peak', 'MS_Peak'], path:str|None=None, **kwargs) -> None:
+
+        '''
+        Visualizes two mass spectra as m/z/intensity plot.
+
+        Args:
+            peaks(tuple['MS_Peak', 'MS_Peak']): Tuple of MS_Peak objects containing the mass spectra to visualize.
+            path (str|None, optional): Path to save the figure to. Defaults to None.
+            **kwargs: Keyword arguments for the plot.
+        '''
+
+        peak1, peak2 = peaks
+        xlim = kwargs.pop('xlim', [None, None])
+        ylim = kwargs.pop('ylim', [None, None])
+        colors = kwargs.pop('colors', ('#005573', '#e04214'))
+
+        fig, ax = plt.subplots(figsize=(14.4, 4.8))
+        ax.bar(peak1.mass_spectrum['mz'], peak1.mass_spectrum['rel_intensity'], width=0.05, color=colors[0],
+               edgecolor=colors[0],
+               **kwargs)
+        ax.bar(peak2.mass_spectrum['mz'], peak2.mass_spectrum['rel_intensity']*(-1), width=0.05, color=colors[1],
+               edgecolor=colors[1],
+               **kwargs)
+
+        lable_indices1 = np.argpartition(peak1.mass_spectrum['rel_intensity'], -4)[-4:]
+        lable_indices2 = np.argpartition(peak2.mass_spectrum['rel_intensity'], -4)[-4:]
+
+        for index in lable_indices1:
+            ax.annotate(f'{peak1.mass_spectrum["mz"][index]:.0f}',
+                        (peak1.mass_spectrum['mz'][index], peak1.mass_spectrum['rel_intensity'][index]),
+                        textcoords="offset points", xytext=(0, 5), ha='center', color='darkgrey', fontsize=10)
+            print(peak1.mass_spectrum['rel_intensity'][index])
+
+        for index in lable_indices2:
+            ax.annotate(f'{peak2.mass_spectrum["mz"][index]:.0f}',
+                        (peak2.mass_spectrum['mz'][index], peak2.mass_spectrum['rel_intensity'][index]*(-1)),
+                        textcoords="offset points", xytext=(0, -10), ha='center', color='darkgrey', fontsize=10)
+            print(peak2.mass_spectrum['rel_intensity'][index])
+
+        ax.set_xlim(xlim)
+        ax.set_ylim(ylim)
+
+        plt.grid(color='lightgrey', linestyle='--', which='both', axis='y')
+        ax.set_xlabel('m/z')
+        ax.set_ylabel('Relative Intensity [%]')
+        ax.set_yticks([-100, -75, -50, -25, 0, 25, 50, 75, 100])
+        ax.set_yticklabels([100, 75, 50, 25, 0, 25, 50, 75, 100])
+        ax.spines[['right', 'top']].set_visible(False)
+        fig.tight_layout()
+
+        if path:
+            plt.savefig(path, dpi=400)
+            plt.close()
+        else:
+            plt.show()
+
 
 if __name__ == '__main__':
     array_8x12 = np.random.randint(0, 95, size=(8, 12))
