bc3eb9b301fa9695e8a8ebed68046953a5d0843c
Felix Katzenburg <f_katz01@uni-muenster.de>
2024-01-31 00:08:37 -0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@ release = '0.0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx_rtd_theme']
 
 templates_path = ['_templates']
 exclude_patterns = []
