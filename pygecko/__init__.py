e73e3216d2b8b26ab707d23c5fa3647a87831f2e
Felix Katzenburg <f_katz01@uni-muenster.de>
2024-02-12 11:39:43 -0500

    Update __init__

--- pygecko/__init__.py
+++ pygecko/__init__.py
@@ -11,18 +11,19 @@ __version__ = '0.1.0'
 __author__ = 'Felix Katzenburg'
 __credits__ = 'Muenster University'
 
-config = configparser.ConfigParser()
-config_path = Path(__file__).parent.joinpath('config.ini')
-config.read(config_path)
-msconvert_exe = config.get('msConvertSettings','exe_path')
-if not msconvert_exe:
-    msconvert_exe = input("Please provide the path to the msConvert executable or specify it in the config.ini:")
-    if pathlib.Path.exists(Path(msconvert_exe)):
-        config['msConvertSettings']['exe_path'] = str(msconvert_exe)
-        with open(config_path, "w") as file_object:
-            config.write(file_object)
+if __name__ == '__main__':
+    config = configparser.ConfigParser()
+    config_path = Path(__file__).parent.joinpath('config.ini')
+    config.read(config_path)
+    msconvert_exe = config.get('msConvertSettings','exe_path')
+    if not msconvert_exe:
+        msconvert_exe = input("Please provide the path to the msConvert executable or specify it in the config.ini:")
+        if pathlib.Path.exists(Path(msconvert_exe)):
+            config['msConvertSettings']['exe_path'] = str(msconvert_exe)
+            with open(config_path, "w") as file_object:
+                config.write(file_object)
+        else:
+            raise FileNotFoundError("msConvert executable not found at the specified location.")
     else:
-        raise FileNotFoundError("msConvert executable not found at the specified location.")
-else:
-    if not pathlib.Path.exists(Path(msconvert_exe)):
-        raise FileNotFoundError("msConvert executable not found at the specified location. Please check the path to the msConvert executable or specify it in the config.ini.")
+        if not pathlib.Path.exists(Path(msconvert_exe)):
+            raise FileNotFoundError("msConvert executable not found at the specified location. Please check the path to the msConvert executable or specify it in the config.ini.")
